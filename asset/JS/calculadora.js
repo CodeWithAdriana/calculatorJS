8848d7402c91ca2cf3aa06bebd1db993bd4e782a
Adri <adrianareyesgarcia1@gmail.com>
2024-03-01 10:45:04 -0600

    3thcommit

---
+++ asset/JS/calculadora.js
@@ -0,0 +1,16 @@
+let precioInicial = 400000;
+let cantidadInicial = 0;
+let valorTotal = 0;
+
+
+let precio = document.querySelector(".precio-inicial");
+let cantidad = document.querySelector(".cantidad");
+let total = document.querySelector(".valor-total");
+
+precio.innerHTML = precioInicial;
+cantidad.innerHTML = cantidadInicial;
+total.innerHTML = valorTotal;
+
+
+
+
